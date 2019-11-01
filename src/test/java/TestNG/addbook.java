023e600de823a8acb887bb9c6c37f864bf2b455c
neelimas <neelimas@Whisper.rw.local>
2019-11-01 16:44:00 +1100

    correcting errors

--- src/test/java/TestNG/addbook.java
+++ src/test/java/TestNG/addbook.java
@@ -11,25 +11,26 @@ import org.testng.annotations.Test;
 
 public class addbook {
 	WebDriver driver;
+	
   @BeforeMethod
   public void launch() {
 	  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-	  ChromeDriver driver = new ChromeDriver();
+	  driver = new ChromeDriver();
 	  driver.get("http://www.riskwizard.com");
 	  driver.manage().window().maximize();
-	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	  //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
   }
-  
   @Test
   public void verifyaddbook() {
+	  
 	  String x = driver.getTitle();
 		System.out.println(x);
 		Assert.assertEquals(x, x);
   }
   
   @AfterMethod
-  public void close(){
-	  driver.close();	  
+  public void close() {
+	  driver.close();  
   }
 }
 
